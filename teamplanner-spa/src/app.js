51d31af1127ff688606b951b5323db4184cb8ccc
Jan Syring-Lingenfelder <github@jan-sl.de>
2020-09-10 15:38:50 +0200

    Revert "feat(spa): loading indicator"
    This reverts commit a813517396988bb40d7eed8e67ea16e742fcd841.
    The loading indicator will be replaced with a pull to refresh

--- teamplanner-spa/src/app.js
+++ teamplanner-spa/src/app.js
@@ -9,7 +9,6 @@ export class App extends LitElement {
       matches: { type: Array },
       votes: { type: Array },
       isOffline: { type: Boolean },
-      isLoading: { type: Boolean },
     };
   }
 
@@ -19,7 +18,6 @@ export class App extends LitElement {
     this.matches = [];
     this.votes = [];
     this.isOffline = !navigator.onLine;
-    this.isLoading = true;
 
     window.addEventListener('online', () => { this.isOffline = false; this.fetchData() })
     window.addEventListener('offline', () => this.isOffline = true)
@@ -30,23 +28,16 @@ export class App extends LitElement {
   }
 
   fetchData() {
-    this.isLoading = true;
-
-    Promise.all([
-      fetch('/matches', { headers: { 'Accept': 'application/json'}})
-        .then(r => r.json())
-        .then(r => r.filter(v => moment(v.date).isSameOrAfter(moment(), 'day')))
-        .then(r => { this.matches = r }),
-      fetch('/teammates', { headers: { 'Accept': 'application/json'}})
-        .then(r => r.json())
-        .then(r => { this.teammates = r }),
-      fetch('/votes', { headers: { 'Accept': 'application/json'}})
-        .then(r => r.json())
-        .then(r => { this.votes = r })
-    ]).catch(error => { alert('Daten konnten nicht abgerufen werden.'); console.log(error) })
-    .finally(() => {
-      this.isLoading = false
-    })
+    fetch('/matches', { headers: { 'Accept': 'application/json'}})
+      .then(r => r.json())
+      .then(r => r.filter(v => moment(v.date).isSameOrAfter(moment(), 'day')))
+      .then(r => { this.matches = r })
+    fetch('/teammates', { headers: { 'Accept': 'application/json'}})
+      .then(r => r.json())
+      .then(r => { this.teammates = r })
+    fetch('/votes', { headers: { 'Accept': 'application/json'}})
+    .then(r => r.json())
+    .then(r => { this.votes = r })
   }
 
   voteChange(event) {
@@ -107,23 +98,6 @@ export class App extends LitElement {
         font-size: smaller;
         background-color: #aaa;
       }
-
-      #spinner {
-        border: 16px solid #aaa;
-        border-top: 16px solid #ccc;
-        border-radius: 50%;
-        width: 100px;
-        height: 100px;
-        animation: spin 2s linear infinite;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-      }
-
-      @keyframes spin {
-        0% { transform: translate(-50%, -50%) rotate(0deg); }
-        100% { transform: translate(-50%, -50%) rotate(360deg); }
-      }
     `;
   }
 
@@ -137,7 +111,6 @@ export class App extends LitElement {
         <h1>Teamplaner</h1>
       </header>
       <main>
-        ${this.isLoading ? html`<div id="spinner"></div>` :''}
         <vote-matrix .teammates=${this.teammates} .matches=${this.matches} .votes=${this.votes} @vote-change=${this.voteChange}></vote-matrix>
       </main>
 
